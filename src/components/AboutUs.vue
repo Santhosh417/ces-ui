6d9d39686bc3499c35278e10d2e30165d39a24fb
ShravyaChandiri <schandiri@unomaha.edu>
2021-04-03 18:06:51 -0500

    corrected responsiveness

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -1,40 +1,35 @@
 <template>
-  <b-container>
-    
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <img src="https://www.unomaha.edu/news/2020/03/img/ayailovenu-f.jpg" alt="Student" style="width:100%">
-          </div>
-          <div class="col">
-            <h2><b>MISSION</b></h2>
-            <p><b>As both a Metropolitan University of distinction and a Carnegie Doctoral Research institution, the University of Nebraska at Omaha (UNO) transforms and improves the quality of life locally, nationally, and globally.</b></p>
-            
-          </div>
-        </div>
+  <b-container fluid>
+        <b-row class="mt-5">
+          <b-col>
+            <b-img src="https://www.unomaha.edu/news/2020/03/img/ayailovenu-f.jpg" alt="Student" fluid/>
+          </b-col>
+          <b-col>
+            <h3><b>MISSION</b></h3>
+            <p>As both a Metropolitan University of distinction and a Carnegie Doctoral Research institution, the University of Nebraska at Omaha (UNO) transforms and improves the quality of life locally, nationally, and globally.</p>
 
-        <div class="row">
-          <div class="col">
-            <h2><b>VISION</b></h2>
-            <p><b>The University of Nebraska at Omaha is recognized as the premier Metropolitan University throughout the United States and the world.</b></p>
-          </div>
-          <div class="col">
-            <img src="https://unothegateway.com/wp-content/uploads/2020/03/criss-libaray_ucomm-scaled.jpg" alt="Library" style="width:100%">
-          </div>
-        </div>
+          </b-col>
+        </b-row>
 
-        <div class="row">
-          <div class="col">
-            <img src="https://www.unomaha.edu/college-of-business-administration/executive-mba/mini-mba/20180920_mini_mba_012.jpg" alt="Classroon" style="width:100%">
-          </div>
-          <div class="col">
+        <b-row class="mt-5">
+          <b-col>
+            <h3><b>VISION</b></h3>
+            <p>The University of Nebraska at Omaha is recognized as the premier Metropolitan University throughout the United States and the world.</p>
+          </b-col>
+          <b-col>
+            <b-img src="https://unothegateway.com/wp-content/uploads/2020/03/criss-libaray_ucomm-scaled.jpg" alt="Library" fluid/>
+          </b-col>
+        </b-row>
+
+    <b-row class="mt-5">
+      <b-col>
+            <b-img src="https://www.unomaha.edu/college-of-business-administration/executive-mba/mini-mba/20180920_mini_mba_012.jpg" alt="Classroon" fluid/>
+          </b-col>
+          <b-col>
             <h2><b>HISTORY</b></h2>
-            <p><b>The history of UNO has been a powerful guiding force. Our past fuels our pride and shapes the comprehensive metropolitan university we've become. We were built on a commitment to provide a rigorous, relevant and affordable education that meets the diverse needs of traditional and nontraditional students. UNO has been and always will be driven by the extraordinary connection to and partnership with the city of Omaha.</b></p>
-          </div>
-        </div>
-      </div>
-
-
+            <p>The history of UNO has been a powerful guiding force. Our past fuels our pride and shapes the comprehensive metropolitan university we've become. We were built on a commitment to provide a rigorous, relevant and affordable education that meets the diverse needs of traditional and nontraditional students. UNO has been and always will be driven by the extraordinary connection to and partnership with the city of Omaha.</p>
+          </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -45,18 +40,39 @@ name: "AboutUs"
 </script>
 
 <style scoped>
+@media only screen
+and (device-width : 375px)
+and (device-height : 812px)
+and (-webkit-device-pixel-ratio : 3) {
 
-.row {
-  text-align: center;
+  .h6 {
+    font-size: 2vw;
+  }
+
+  .h3 {
+    font-size: 5vw;
+    font-weight: bold;
+  }
+
+  .h4{
+    font-size: 4vw;
+  }
+
+  a{
+    font-size: 3vw;
+  }
+
+  p{
+    font-size: 3vw;
+  }
+
+  b{
+    font-size: 5vw;
+    font-weight: bold;
+  }
+
+  h6 {
+    font-size: 2vw !important;
+  }
 }
-
-.col {
-  float: left;
-  width: 33.33%;
-  padding: 40px;
-  text-align: center;
-  
-}
-
-
 </style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,23 +1,25 @@
 <template>
   <b-container>
-    <b-row >
-      <b-col class="col-sm-7"></b-col>
-      <b-col v-if="credentials.authenticates" class="col-sm-5 mb-sm-3 mt-sm-3 float-right">
+        <b-card v-if="credentials.authenticates" class="mt-3 mb-3 bg-light">
+        <b-row>
+          <b-col>
+            <p><strong>Enter student NUID</strong></p>
+          </b-col>
+        </b-row>
         <b-row>
           <b-col class="col-sm-8">
             <b-input-group class="mb-sm-2">
               <b-input-group-prepend is-text>
                 <b-icon icon="search"></b-icon>
               </b-input-group-prepend>
-              <b-form-input type="search" v-model="studentID" placeholder="Enter Student NUID" @input="validateStudentID"/>
+              <b-form-input type="search"  v-model="studentID" placeholder="NUID" @input="validateStudentID"/>
             </b-input-group>
           </b-col>
           <b-col class="col-sm-4">
             <b-button :to="{name:'student', params:{studentID : studentID}}" class="btn-primary">Search</b-button>
           </b-col>
         </b-row>
-      </b-col>
-    </b-row>
+        </b-card>
       <div class="carousel">
         <b-carousel
           id="carousel-1"
@@ -49,22 +51,22 @@
         </b-carousel>
       </div>
       <div class="content">
-        <p><b>The University of Nebraska Omaha (Omaha or UNO) is a public research university in Omaha, Nebraska. Founded in 1908 by faculty from the Omaha Presbyterian Theological Seminary as a private non-sectarian college, the university was originally known as the University of Omaha.</b></p>
-        <p><b>UNO is Nebraska's metropolitan university — a university with strong academic values and significant relationships with our community that transforms and improves the lives of those on a local, regional, national, and international level.</b></p>
+        <p>The University of Nebraska Omaha (Omaha or UNO) is a public research university in Omaha, Nebraska. Founded in 1908 by faculty from the Omaha Presbyterian Theological Seminary as a private non-sectarian college, the university was originally known as the University of Omaha.</p>
+        <p>UNO is Nebraska's metropolitan university — a university with strong academic values and significant relationships with our community that transforms and improves the lives of those on a local, regional, national, and international level.</p>
       </div>
-      <div class="row">
-        <div class="column">
+      <b-row>
+        <b-col>
           <img src="https://www.unomaha.edu/news/2020/03/img/ayailovenu-f.jpg" alt="Student" style="width:100%">
-        </div>
-        <div class="column">
+        </b-col>
+        <b-col>
           <img src="https://unothegateway.com/wp-content/uploads/2020/03/criss-libaray_ucomm-scaled.jpg" alt="Library" style="width:100%">
-        </div>
-        <div class="column">
-          <img src="https://www.unomaha.edu/college-of-arts-and-sciences/mathematics/_files/images/math%20lab2.JPG" alt="Classroom" style="width:100%">
-        </div>
-      </div>
+        </b-col>
+          <b-col>
+          <b-img src="https://www.unomaha.edu/college-of-arts-and-sciences/mathematics/_files/images/math%20lab2.JPG" alt="Classroom" style="width:100%"/>
+          </b-col>
+      </b-row>
       <div class="content">
-        <p><b>UNO is dedicated to the city and state in our name. As the University of Nebraska's metropolitan university campus, no fences or barriers separate students from the opportunities offered by the greater Omaha area. We address real issues, providing relevant learning opportunities that uniquely prepare our graduates as professionals and active members of their community.</b></p>
+        <p>UNO is dedicated to the city and state in our name. As the University of Nebraska's metropolitan university campus, no fences or barriers separate students from the opportunities offered by the greater Omaha area. We address real issues, providing relevant learning opportunities that uniquely prepare our graduates as professionals and active members of their community.</p>
       </div>
   </b-container>
 </template>
@@ -118,19 +120,45 @@ export default {
 
 }
 
-.column {
-  float: left;
-  width: 33.33%;
-  padding: 5px;
-}
-
-
 .btn-primary{
   background-color: #000000;
   color: white;
 }
 
-.search-element{
-  background-color: grey;
+
+@media only screen
+and (device-width : 375px)
+and (device-height : 812px)
+and (-webkit-device-pixel-ratio : 3) {
+
+  .h6 {
+    font-size: 2vw;
+  }
+
+  .h3 {
+    font-size: 5vw;
+    font-weight: bold;
+  }
+
+  .h4{
+    font-size: 4vw;
+  }
+
+  a{
+    font-size: 3vw;
+  }
+
+  p{
+    font-size: 3vw;
+  }
+
+  b{
+    font-size: 5vw;
+    font-weight: bold;
+  }
+
+  h6 {
+    font-size: 2vw !important;
+  }
 }
 </style>

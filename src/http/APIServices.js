67dc54750c474084978dc8db725ec43da5bb5910
Santhosh <santhosh.veeramalla@mutualofomaha.com>
2021-04-26 15:26:29 -0500

    Point UI application to ces-service

--- src/http/APIServices.js
+++ src/http/APIServices.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import axios from 'axios';
 // const API_URL = 'http://127.0.0.1:8000/';
-const API_URL = 'https://ces-service-test.herokuapp.com/';
+const API_URL = 'https://ces-service.herokuapp.com/';
 export class APIService {
   constructor() {
   }
